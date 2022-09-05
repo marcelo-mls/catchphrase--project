59f82ab135fd048882d49a22a695610d7f6789aa
Marcelo Marques <marcelo-mls@hotmail.com>
2022-09-05 02:46:12 -0300

    Refatorando - Component App agora é funcional

--- src/App.js
+++ src/App.js
@@ -1,63 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Words from './components/Words';
 import './styles/App.css';
 import sunIcon from './images/sun.png';
 import moonIcon from './images/moon.png';
 import { BsSunFill, BsMoonFill, BsGrid } from "react-icons/bs";
 
-// menu: https://cdn-icons-png.flaticon.com/512/6423/6423878.png
-// dark mode: https://cdn-icons-png.flaticon.com/512/702/702471.png
-// light mode: https://cdn-icons-png.flaticon.com/512/66/66275.png
+function App() {
+  const [screenMode, setScreenMode] = useState('default')
+  const [screenModeIcon, setScreenModeIcon] = useState(moonIcon)
 
-class App extends React.Component {
-  state = {
-    screenMode: 'default',
-    screenModeIcon: moonIcon, 
+  const toggleScreenMode = () => {
+    const toggleScreenMode = screenMode === 'default'
+      ? ['dark', sunIcon]
+      : ['default', moonIcon]
+
+    setScreenMode(toggleScreenMode[0])
+    setScreenModeIcon(toggleScreenMode[1])
   }
 
-  setScreenMode = () => {
-    const { screenMode } = this.state;
-    const toggleScreenMode =
-      screenMode === 'default' ? ['dark', sunIcon] : ['default', moonIcon]
-    this.setState({
-      screenMode: toggleScreenMode[0],
-      screenModeIcon: toggleScreenMode[1]
-    })
-  }
-
-  render (){
-    const { screenMode, screenModeIcon } = this.state;
-    return (
-      <div className={ `App ${screenMode}` }>
-        <header>
-          <BsGrid className='icons' style={{fontSize: '36px'}}/>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/6423/6423878.png"
-            alt="menu"
-          />
-          <button>Catchphrase Game</button>
-          <BsMoonFill className='icons' style={{fontSize: '36px'}}/>
-          <BsSunFill className='icons' style={{fontSize: '36px'}}/>
-          <img
-            onClick={ this.setScreenMode }
-            src={ screenModeIcon }
-            alt="dark mode" 
-          />
-        </header>
-        <Words />
-        <footer>
-          developed by
-          {' '}
-          <a
-            href='https://www.linkedin.com/in/marcelo-mls/'
-            target='_blank'
-            rel='noopener noreferrer'>@marcelo-mls</a>
-          {' '}
-          with React
-        </footer>
-      </div>
-    );
-  }
+  return (
+    <div className={`App ${screenMode}`}>
+      <header>
+        <BsGrid className='icons' style={{ fontSize: '36px' }} />
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/6423/6423878.png"
+          alt="menu"
+        />
+        <button>Catchphrase Game</button>
+        <BsMoonFill className='icons' style={{ fontSize: '36px' }} />
+        <BsSunFill className='icons' style={{ fontSize: '36px' }} />
+        <img
+          onClick={toggleScreenMode}
+          src={screenModeIcon}
+          alt="dark mode"
+        />
+      </header>
+      <Words />
+      <footer>
+        developed by
+        {' '}
+        <a
+          href='https://www.linkedin.com/in/marcelo-mls/'
+          target='_blank'
+          rel='noopener noreferrer'>@marcelo-mls</a>
+        {' '}
+        with React
+      </footer>
+    </div>
+  );
 }
 
 export default App;
